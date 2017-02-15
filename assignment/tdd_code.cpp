d6f2551222e9a43f1ee1f87e92165619182b6891
Dominik Harmim <harmim6@gmail.com>
2017-02-15 21:53:24 +0100

    Initial commit

---
+++ assignment/tdd_code.cpp
@@ -0,0 +1,65 @@
+//======== Copyright (c) 2017, FIT VUT Brno, All rights reserved. ============//
+//
+// Purpose:     Test Driven Development - priority queue code
+//
+// $NoKeywords: $ivs_project_1 $tdd_code.cpp
+// $Author:     JMENO PRIJMENI <xlogin00@stud.fit.vutbr.cz>
+// $Date:       $2017-01-04
+//============================================================================//
+/**
+ * @file tdd_code.cpp
+ * @author JMENO PRIJMENI
+ * 
+ * @brief Implementace metod tridy prioritni fronty.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "tdd_code.h"
+
+//============================================================================//
+// ** ZDE DOPLNTE IMPLEMENTACI **
+//
+// Zde doplnte implementaci verejneho rozhrani prioritni fronty (Priority Queue)
+// 1. Verejne rozhrani fronty specifikovane v: tdd_code.h (sekce "public:")
+//    - Konstruktor (PriorityQueue()), Destruktor (~PriorityQueue())
+//    - Metody Insert/Remove/Find a GetHead
+//    - Pripadne vase metody definovane v tdd_code.h (sekce "protected:")
+//
+// Cilem je dosahnout plne funkcni implementace prioritni fronty implementovane
+// pomoci tzv. "double-linked list", ktera bude splnovat dodane testy 
+// (tdd_tests.cpp).
+//============================================================================//
+
+PriorityQueue::PriorityQueue()
+{
+
+}
+
+PriorityQueue::~PriorityQueue()
+{
+
+}
+
+void PriorityQueue::Insert(int value)
+{
+
+}
+
+bool PriorityQueue::Remove(int value)
+{
+    return false;
+}
+
+PriorityQueue::Element_t *PriorityQueue::Find(int value)
+{
+    return NULL;
+}
+
+PriorityQueue::Element_t *PriorityQueue::GetHead()
+{
+    return NULL;
+}
+
+/*** Konec souboru tdd_code.cpp ***/
